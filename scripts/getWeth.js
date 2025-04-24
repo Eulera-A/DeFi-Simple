f6171d855705bc1adeb08c2a603d903eb8e6e8b1
Eulera <feiliujustina@hotmail.com>
2025-04-23 22:48:44 -0700

    signer object fixed

--- scripts/getWeth.js
+++ scripts/getWeth.js
@@ -3,6 +3,8 @@ const AMOUNT = ethers.utils.parseEther("0.02");
 
 async function getWeth() {
   const { deployer } = await getNamedAccounts();
+  const signer = await ethers.getSigner(deployer); // 🔥 FIX HERE, need to get the Signer object, not just an address
+
   // call the "deposit" function on the weth contract
   // need: abi, contract adress from weth mainnet: but ofcourse, we are testing/working on the forked mainnet
   // 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2
@@ -10,7 +12,7 @@ async function getWeth() {
   const iWeth = await ethers.getContractAt(
     "IWeth", // the iweth abi
     "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2", //address
-    deployer //
+    signer //the signer object! which is the deployer
   );
   const tx = await iWeth.deposit({ value: AMOUNT });
   await tx.wait(); // because deposit changes blockchain status,
